aa91979b1d619fbc68cec706ebb6fd47188d00a1
Zara <zara@okaayfinee.home>
2020-02-09 03:02:37 +1100

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,4 +1,25 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 const cardsInPlay = [];
 
 function checkForMatch() {
@@ -10,8 +31,10 @@ function checkForMatch() {
 }
 
 function flipCard(cardID) {
-cardsInPlay.push(cards[cardID]);
-console.log("User flipped " + cards[cardID])
+cardsInPlay.push(cards[cardID].rank);
+console.log("User flipped " + cards[cardID].rank)
+console.log("User flipped " + cards[cardID].suit)
+console.log("User flipped " + cards[cardID].cardImage)
 checkForMatch();
 }
 
