b536ef6d4e8b34af3772bb9c7696e07956a9f06f
Zara <zara@okaayfinee.home>
2020-02-09 02:05:36 +1100

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,19 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "king";
-let cardFour = "queen";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
 
+const cardOne = cards[0];
+cardsInPlay.push('cardOne');
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+const cardTwo = cards[3];
+cardsInPlay.push('cardTwo');
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else if (cardsInPlay[0] !== cardsInPlay[1]) {
+		alert("Sorry, try again!");
+	}
+}
